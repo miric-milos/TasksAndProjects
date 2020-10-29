cead04476ef0a2413b2d727954cadf7b8db27a03
Miloš Mirić <56596844+miric-milos@users.noreply.github.com>
2020-10-29 16:30:51 +0100

    Add project files.

---
+++ TasksAndProjectsApp/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasksAndProjectsApp.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        // login info
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public Role Role { get; set; }
+    }
+}
